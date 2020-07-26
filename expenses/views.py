cf0d3f46bc1f90c928e47d9849e48a0aca495d82
SeanFromOhio <seanohioroberts@gmail.com>
2020-07-26 09:41:02 -0400

    setting up a tester template

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, "swerl/index.html", {})
+
+
+def post_expenses(request):
+    return render(request, "expenses/expenses.html", {})
