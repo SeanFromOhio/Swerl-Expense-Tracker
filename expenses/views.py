0533cb08da25ca0ebf426867472af49ec5d80198
SeanFromOhio <seanohioroberts@gmail.com>
2020-07-29 19:46:42 -0400

    Correctly adds to the Expenses DB, but still need to figure out best way to do the date entry. Next is to create the profile page.

--- expenses/views.py
+++ expenses/views.py
@@ -13,25 +13,33 @@ def expenses_page(request):
 
 
 def post_expense(request):
-    if request.user.is_authenticated:
+    if request.user.username:
         if request.method == "POST":
 
-            expense_amount = request.POST.get("amount")
-            expense_date = request.POST.get("expense_date")
+            expense_amount = request.POST.get("expense_amount")
+            # expense_date = request.POST.get("expense_date")
             expense_description = request.POST.get("description")
             expense_type = request.POST.get("expense_type")
-            expense_author = request.username.user
-            print(expense_type)
+            expense_author = request.user.username
 
-            expense_entry = Expense(
-                amount=expense_amount,
-                expense_date=expense_date,
-                description=expense_description,
-                expense_type=expense_type,
-                author=User.objects.get(username=expense_author)
-            )
-            expense_entry.save()
-        return redirect("/")
+            expense_call = Expense()
+            expense_call.amount = expense_amount
+            # expense_call.expense_date = expense_date
+            expense_call.description = expense_description
+            expense_call.expense_type = expense_type
+            expense_call.author = User.objects.get(username=expense_author)
+
+            expense_call.save()
+
+            # expense_entry = Expense(
+            #     amount=expense_amount,
+            #     expense_date=expense_date,
+            #     description=expense_description,
+            #     expense_type=expense_type,
+            #     author=User.objects.get(username=expense_author)
+            # )
+            # expense_entry.save()
+            return redirect("/")
 
     else:
         return HttpResponseRedirect("/signup")
