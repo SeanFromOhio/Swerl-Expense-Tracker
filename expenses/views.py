07659adb205158e19012e7ec4eb92ae2467bfa14
SeanFromOhio <seanohioroberts@gmail.com>
2020-08-12 17:40:04 -0400

    Added a functional plot (pie chart) and have the code to create more. Also added additional css for organization purposes and color scheming.

--- expenses/views.py
+++ expenses/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Expense
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
+from django.db.models import Sum
 from datetime import datetime
+from plotly.graph_objs import Pie, Scatter, Figure
+from plotly.offline import plot
 
 
 def index(request):
@@ -14,9 +17,51 @@ def expenses_page(request):
     author_id = request.user.id
     user_expenses = Expense.objects.filter(author=author_id).order_by("-expense_date")
     user_name = request.user.username
+
+    # ----------- CHARTS ------------
+
+    # Expense pie chart
+    food_amount = Expense.objects.filter(expense_type="food").\
+        filter(author=author_id).aggregate(Sum("amount"))
+    personal_amount = Expense.objects.filter(expense_type="personal").\
+        filter(author=author_id).aggregate(Sum("amount"))
+    transportation_amount = Expense.objects.filter(expense_type="transportation").\
+        filter(author=author_id).aggregate(Sum("amount"))
+    housing_amount = Expense.objects.filter(expense_type="housing").\
+        filter(author=author_id).aggregate(Sum("amount"))
+    other_amount = Expense.objects.filter(expense_type="other").\
+        filter(author=author_id).aggregate(Sum("amount"))
+
+    # Names of the pie chart segments
+    groups = ["Food", "Personal", "Transportation", "Housing", "Other"]
+
+    # Amounts of the pie chart segments
+    amounts = [food_amount["amount__sum"], personal_amount["amount__sum"], transportation_amount["amount__sum"],
+               housing_amount["amount__sum"], other_amount["amount__sum"]]
+
+    # Segment colors
+    colors = ["#C0ECCC", "#F6A8A6", "#F9F0C1", "#A5C8E4", "#F4CDA6"]
+
+    # Custom '$'
+    text = ["$", "$", "$", "$", "$"]
+
+    # The plot creator
+    pie_plot = Figure(Pie(
+                    labels=groups, values=amounts,
+                    hoverinfo="label+percent", text=text, textinfo="text+value",
+                    textfont=dict(size=15),
+                    hole=.3,
+                    marker=dict(colors=colors,
+                                line=dict(color="#000000", width=3)
+                                )))
+
+    # This is needed to view the plot in a localized setting (not online)
+    plot_div = plot(pie_plot, output_type="div")
+
     context = {
         "user_expenses": user_expenses,
         "user_name": user_name,
+        "plot_div": plot_div,
     }
     return render(request, "expenses/expenses.html", context)
 
@@ -44,3 +89,4 @@ def post_expense(request):
 
     else:
         return HttpResponseRedirect("/signup")
+
