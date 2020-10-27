a46c7d7d8b1ac5bc2297a3b50636a05e496f8ec7
SeanFromOhio <seanohioroberts@gmail.com>
2020-10-27 13:03:00 -0400

    Updated requirements to be added to heroku. Deploying to heroku.

--- Swerl/settings.py
+++ Swerl/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,3 +129,6 @@ STATICFILES_DIRS = [
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
